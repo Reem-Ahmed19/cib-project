94720ed324d6d6a9a78e8615296e8f8f23b8f33d
Reem Ahmed <23-101100@students.eui.edu.eg>
2026-02-13 19:45:20 +0200

    five

---
+++ src/components/common/cardsec4.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './cardsec4.css'
+import imgsec4 from '../../assets/pic5.png'
+const Cardsec4 = () => {
+    return ( <>
+<div className='cardsec4'>
+<img className='imgsec4' src={imgsec4} alt="" />
+< p className='titlesec4'>Buying and making a home</p>
+< p className='subtitlesec4'>Buying a home can be an emotional process, but itrsquo;s important to approach it logically </p>
+<p className='btnsec4'>Read more</p>
+    
+</div>
+
+
+    </> );
+}
+ 
+export default Cardsec4;
---
+++ src/components/common/navsec2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../common/navsec2.css'
+
+const Navigationlinksec2 = () => {
+    return ( 
+        <nav className='text-link-sec2'>
+            Apply Online
+        </nav>
+     );
+}
+ 
+export default Navigationlinksec2;
